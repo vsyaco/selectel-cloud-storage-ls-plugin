39d32368a4c3b76b41bd5b270a96eb73061c6167
kexek <mail@kexek.me>
2013-01-27 19:56:57 +0700

    changing upload method for more compatibility

--- classes/modules/image/Image.class.php
+++ classes/modules/image/Image.class.php
@@ -14,7 +14,7 @@ class PluginSelectelstorage_ModuleImage extends PluginSelectelstorage_Inherit_Mo
     /*
      * Сохранение в Selectel Storage
      */
-    public function UploadToSelectelstorage($sFilePath) {
+    public function UploadToSelectelstorage($sFilePath,$sFileSource=NULL) {
 
         if (extension_loaded('curl')) {
 
@@ -22,10 +22,16 @@ class PluginSelectelstorage_ModuleImage extends PluginSelectelstorage_Inherit_Mo
 
             try {
 
-				$bucket = $sS->getContainer(Config::Get('plugin.selectelstorage.bucket'));
-				$sName = strtolower(array_pop(explode(trim(Config::Get('path.uploads.root')).'/', $sFilePath)));
+                $bucket = $sS->getContainer(Config::Get('plugin.selectelstorage.bucket'));
 
-				if ($bucket->putFile($sFilePath, $sName)) {
+                //$sName = strtolower(array_pop(explode(trim(Config::Get('path.uploads.root')).'/', $sFilePath)));
+                // changed this because of PHP strict standarts “only variables should be passed by reference”
+                $exploded_filepath = explode(trim(Config::Get('path.uploads.root')).'/', $sFilePath);
+                $sName = strtolower(array_pop($exploded_filepath));
+
+                $sFilePath = (isset($sFileSource) ? $sFileSource : $sFilePath);
+
+                if ($bucket->putFile($sFilePath, $sName)) {
                     @unlink($sFilePath);
                     return 'http://' . Config::Get('plugin.selectelstorage.domain') . '/'. $sName;
                 }
@@ -33,7 +39,7 @@ class PluginSelectelstorage_ModuleImage extends PluginSelectelstorage_Inherit_Mo
                     error_log('Something wrong while uploading file to selectel storage');
                     return false;
                 }
-			}
+            }
             catch(Exception $e) {
                 error_log("Something wrong while uploading file to selectel storage. Error: ".$e->getMessage());
                 return false;
@@ -47,15 +53,23 @@ class PluginSelectelstorage_ModuleImage extends PluginSelectelstorage_Inherit_Mo
      * Изменяем логику сохранения файла
      */
     public function SaveFile($sFileSource,$sDirDest,$sFileDest,$iMode=null,$bRemoveSource=false) {
+
         $sFileDestFullPath=rtrim(Config::Get('path.root.server'),"/").'/'.trim($sDirDest,"/").'/'.$sFileDest;
+        $sFileDestFullPath=trim($sDirDest,"/").'/'.$sFileDest;
+
         $this->CreateDirectory($sDirDest);
 
-        $bResult=copy($sFileSource,$sFileDestFullPath);
+        //$bResult=copy($sFileSource,$sFileDestFullPath);
+        $bResult = true;
+
+        //$fileContent = file_get_contents($sFileSource);
+        //$bResult=file_put_contents($sFileDestFullPath, $fileContent);
+
         if ($bResult and !is_null($iMode)) {
             chmod($sFileDestFullPath,$iMode);
         }
         if(!strpos($sDirDest,'/tmp/avatars/') && !strpos($sDirDest,'/tmp/fotos/')) {
-            $sFileDestFullPath = $this->UploadToSelectelStorage($sFileDestFullPath);
+            $sFileDestFullPath = $this->UploadToSelectelStorage($sFileDestFullPath,$sFileSource);
         }
 
         if ($bRemoveSource) {
